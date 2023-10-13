154954be39af542f2e8630632d9665f4a619769e
AnitRodriguez <anitruthrodriguez@gmail.com>
2023-10-13 06:11:59 -0700

    hello

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
  * main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
  *
- * Return: Always 0.
+ * Return: 0.
  */
 int main(void)
 {
