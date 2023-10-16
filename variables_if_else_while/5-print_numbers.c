2d9cef4b3669a445eda617b0cd98687a68775968
AnitRodriguez <anitruthrodriguez@gmail.com>
2023-10-16 08:12:59 -0700

    5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *main - Write a program that prints all single digit numbers
-of base 10 starting from 0, followed by a new line.
+*of base 10 starting from 0, followed by a new line.
 *Return: (0)
 */
 int main(void)
