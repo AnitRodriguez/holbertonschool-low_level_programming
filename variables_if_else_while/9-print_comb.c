901888c87e74c14c74dfb04b7fe9e54e3b6c30b8
AnitRodriguez <anitruthrodriguez@gmail.com>
2023-10-16 10:14:48 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Write a program that prints all possible combinations
+*of single-digit numbers
+*Return: (0)
+*/
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
