c8d0f0e0210beeef2e416f26f586796e4ba7b248
AnitRodriguez <anitruthrodriguez@gmail.com>
2023-10-17 08:16:55 -0700

    4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  *
  *@c: Parameter to check
  *
- * Return 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 
 int _isalpha(int c)
