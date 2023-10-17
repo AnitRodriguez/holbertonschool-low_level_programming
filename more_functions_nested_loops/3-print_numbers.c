406bb400897b2fec4b9ae31d05cbfe20b3e09a28
AnitRodriguez <anitruthrodriguez@gmail.com>
2023-10-17 16:45:53 -0700

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Checker.
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int x = '0';
+
+	for (; a <= '9'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
