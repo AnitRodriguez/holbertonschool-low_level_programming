19deffa3cc9453339f1ad9cc5f5fb3b2276268e8
AnitRodriguez <anitruthrodriguez@gmail.com>
2023-10-17 17:02:43 -0700

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry piont
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 1;
+
+	for (; a < 100 ; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz ");
+		else if (a % 3 == 0)
+			printf("Fizz ");
+		else if (a % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", x);
+	}
+	printf("Buzz\n");
+	return (0);
+}
