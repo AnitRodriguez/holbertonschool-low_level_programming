cfc65855565fa3316ba541c0f4d6e83b39015821
AnitRodriguez <anitruthrodriguez@gmail.com>
2023-10-23 08:43:36 -0700

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*puts2 - Checker
+*@str: Parameter to check
+*/
+void puts2(char *str)
+{
+	int o = 0, len = 0;
+
+	while (str[len] != '\0')
+			len++;
+
+	len -= 1;
+	for (; o <= len; o += 2)
+		_putchar(str[o]);
+	_putchar('\n');
+}
