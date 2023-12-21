1e0e19ef5c1ef354512df9adab265a73a5573415
AnitRodriguez <anitruthrodriguez@gmail.com>
2023-12-21 12:05:14 -0800

    0. It ain't what they call you, it's what you answer to

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main - Body.
+ *@argv: Argumen value.
+ *@argc: Argument count.
+ *Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
